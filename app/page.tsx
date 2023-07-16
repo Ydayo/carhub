861fa19170eed977ac412e678ecab2e0a979be01
IshikawaTokyo <yyusb.mac@gmail.com>
2023-07-16 23:06:51 +0900

    変更

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,16 @@ import SearchBar from "@/components/SearchBar";
 import Image from "next/image";
 import { fetchCars } from "@/utils";
 import CarCard from "@/components/CarCard";
+import { FilterProps } from "@/types";
 
-export default async function Home() {
-  const allCars = await fetchCars();
+export default async function Home({ searchParams }: FilterProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || "2022",
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || "10",
+    model: searchParams.model || "",
+  });
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
